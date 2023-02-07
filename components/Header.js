1c74ae8858dcd6df3501c6e4d31e02e7b989e79a
PhilLeGoff <webbyphil65@gmail.com>
2023-02-07 17:39:07 +0100

    m

--- components/Header.js
+++ components/Header.js
@@ -26,7 +26,7 @@ function Header() {
   }, []);
 
   const handleRegister = () => {
-    fetch('https://morning-news-lake.vercel.app/users/signup', {
+    fetch('https://morning-news-backend-lilac.vercel.app/users/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username: signUpUsername, password: signUpPassword }),
@@ -42,7 +42,7 @@ function Header() {
   };
 
   const handleConnection = () => {
-    fetch('https://morning-news-lake.vercel.app/users/signin', {
+    fetch('https://morning-news-backend-lilac.vercel.app/users/signin', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username: signInUsername, password: signInPassword }),
